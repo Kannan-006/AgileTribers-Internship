ee1b5c10693614ec451b539d4f70931c23c2333a
Kannan <nagappanramu44@gmail.com>
2025-04-02 13:53:17 +0530

    js task3

---
+++ Js Task-3/Task-3.js
@@ -0,0 +1,6 @@
+const multiply = function(x, y) {
+    return x * y;
+};
+
+console.log(multiply(4, 5));    
+console.log(multiply(3, 7));     
---
+++ Js Task-3/Task-5.js
@@ -0,0 +1,6 @@
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(10, 2));   
+console.log(divide(7, 3));       
